680c4ae4e84e0cf8a5404e90595714a6428f9047
haseshu <haseshu@gmail.com>
2020-11-10 23:53:56 +0900

    creating shopping

--- flask/react-study/src/App.js
+++ flask/react-study/src/App.js
@@ -2,23 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Ranking from './containers/Ranking';
-import { Route, Link } from 'react-router-dom';
+import Nav from './containers/Nav';
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ul>
-          <li><Link to ="/all">すべてのカテゴリ</Link></li>
-          <li><Link to ="/category/2502">パソコン、周辺機器</Link></li>
-          <li><Link to ="/category/10002">本、雑誌、コミック</Link></li>
-        </ul>
-        <Route path="/all" component = {Ranking} />
-        <Route path="/category/:id"
-        render={
-          ({match}) => <Ranking categoryId={match.params.id} />
-        } 
-        />
+
+        <Nav />
+
+        <Switch>
+          <Route path="/all" component={Ranking} />
+          <Route
+          path="/category/1"
+          render={()=><Redirect to="/all" />}
+          />
+          <Route
+          path="/category/:id"
+          render={
+            ({match})=><Ranking categoryId={match.params.id}
+            />
+          }
+          />
+        </Switch>        
       </div>
     );
   }
---
+++ flask/react-study/src/reducers/Ranking.js
@@ -0,0 +1,41 @@
+const getRanking = responce =>{
+    const ranking = [];
+    const itemLength = responce.ResultSet.totalResultsReturned
+    for(let index = 0;index < itemLength; index++){
+        const item = responce.ResultSet['0'].Result[index + ''];
+        ranking.push({
+            code:item.Code,
+            name:item.Name,
+            url:item.Url,
+            imageUrl:item.Image.Medium
+        })
+    }
+    return ranking;
+};
+
+const initialState ={
+    categoryId:undefined,
+    ranking:undefined,
+    error:false
+};
+
+export default (state =initialState, action) =>{
+    switch(action.type){
+        case 'START_REQUEST':
+        return {
+            categoryId:action.payload.categoryId,
+            ranking:undefined,
+            error:false
+        };
+
+        case 'RECEIVE_DATA':
+            return action.payload.error
+            ?{...state, error:true}
+            :{
+                ...state,
+                ranking:getRanking(action.payload.responce)
+            };
+        
+        default: return state;
+    }
+}
