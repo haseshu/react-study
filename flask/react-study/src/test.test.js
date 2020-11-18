bf2d469982598b09319785041a946ac538fbcbe6
haseshu <haseshu@gmail.com>
2020-11-18 22:47:34 +0900

    add material-ui

--- flask/react-study/src/test.test.js
+++
@@ -1,6 +0,0 @@
-import test from './test';
-
-test('test',()=>{
-    expect(sum(1,2)).toBe(3);
-}
-);
---
+++ flask/react-study/src/test1.test.js
@@ -0,0 +1,6 @@
+import test1 from './test1';
+
+test('test',()=>{
+    expect(test1(1,2)).toBe(3);
+}
+);
